2c1e1c1610a0dfa1dd0a64179904439596ee8a06
Dardo Dupuy <148165990+Daouy1@users.noreply.github.com>
2024-12-27 23:11:54 -0300

    Script para validar las publicaciones

--- js/script.js
+++ js/script.js
@@ -7,6 +7,28 @@ fetch('get_publicaciones.php')
         // Renderizar publicaciones
     });
 
+/* Validación para publicar*/
+document.getElementById('formPublicacion').addEventListener('submit', function (event) {
+    const titulo = document.getElementById('titulo').value.trim();
+    const contenido = document.getElementById('contenido').value.trim();
+    const errorContainer = document.getElementById('errorContainer');
+
+    errorContainer.style.display = 'none';
+    errorContainer.innerHTML = '';
+
+    if (titulo === '' || contenido === '') {
+        event.preventDefault();
+        errorContainer.style.display = 'block';
+        errorContainer.innerHTML = 'Por favor, completa todos los campos obligatorios.';
+    } else if (titulo.length < 5) {
+        event.preventDefault();
+        errorContainer.style.display = 'block';
+        errorContainer.innerHTML = 'El título debe tener al menos 5 caracteres.';
+    }
+});
+
+
+/* Filtro de Busqueda */
 document.getElementById('searchBar').addEventListener('input', function () {
     const query = this.value.toLowerCase();
     const publicaciones = document.querySelectorAll('#publicacionesContainer .publicacion');
@@ -20,3 +42,4 @@ document.getElementById('searchBar').addEventListener('input', function () {
         }
     });
 });
+
