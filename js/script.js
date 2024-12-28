1d9be4b82193a4d37a40898253b666fa609ead1e
Dardo Dupuy <148165990+Daouy1@users.noreply.github.com>
2024-12-27 23:08:55 -0300

    Script para filtro de busqueda

--- js/script.js
+++ js/script.js
@@ -6,3 +6,17 @@ fetch('get_publicaciones.php')
         loadingMessage.style.display = 'none';
         // Renderizar publicaciones
     });
+
+document.getElementById('searchBar').addEventListener('input', function () {
+    const query = this.value.toLowerCase();
+    const publicaciones = document.querySelectorAll('#publicacionesContainer .publicacion');
+
+    publicaciones.forEach(publicacion => {
+        const titulo = publicacion.querySelector('h3').textContent.toLowerCase();
+        if (titulo.includes(query)) {
+            publicacion.style.display = 'block';
+        } else {
+            publicacion.style.display = 'none';
+        }
+    });
+});
